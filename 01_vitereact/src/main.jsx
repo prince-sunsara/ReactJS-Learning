982bf32b2761e878b978462b43709de9abd69704
prince-sunsara <princesunasara977@gmail.com>
2023-12-30 10:21:19 +0530

    learning little bit more about react its component and reactElement

--- 01_vitereact/src/main.jsx
+++ 01_vitereact/src/main.jsx
@@ -2,8 +2,51 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+function MyApp() {
+  return (
+    // <App />
+    <div>
+      <h1>Custom App !</h1>
+    </div>
+  );
+}
+
+// // self create so does not work, react bundler cannot convert it
+const reactElement = {
+  type: 'a',
+  props: {
+    href: 'https://google.com',
+    target: '_blank'
+  },
+  children: 'Click Me to Visit Google'
+}
+
+// // this work because it is html, which after converted by react bundler
+const anotherElement = (
+  <a href='https://google.com' target='_blank'> Click Me to Visit Google</a>
+)
+
+// this works, because we use rules by react
+const element = React.createElement(
+  'a',
+  {
+    href: "http://google.com",
+    target: '_blank'
+  },
+  'Click Me'
+)
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // // they both work
+  <MyApp />
+  // MyApp()
+
+  // // does not work 
+  // reactElement
+
+  // // this gonna work
+  // anotherElement
+
+  // // this gonna work
+  // element
 );
