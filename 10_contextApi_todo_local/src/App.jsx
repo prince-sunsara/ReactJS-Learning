9fa07acdfef543c843405aa252f213778846a9d4
prince-sunsara <princesunasara977@gmail.com>
2024-01-01 22:41:20 +0530

    creating all methods and localstorage work for todo

--- 10_contextApi_todo_local/src/App.jsx
+++ 10_contextApi_todo_local/src/App.jsx
@@ -1,13 +1,59 @@
-import { useState } from 'react'
+import { useState, useId, useEffect } from 'react'
+import { TodoProvider } from './contexts/index'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+  const todoId = useId();
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [...prev, { id: todoId, ...todo }])
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => {
+      prevTodo.id === id ? todo : prevTodo
+    }))
+  }
+
+  const deleteTodo = (id) => {
+    // todos.map(todo => {
+    //   if (todo.id === id) {
+    //     setTodos([...todos.filter(t => t.id !== id)])
+    //   }
+    // })
+    setTodos((prev) => prev.filter(todo => todo.id !== id))
+  }
+  const toggleComplete = (id) => {
+    setTodos(prev => prev.map(todo => todo === id ? { ...todo, completed: !todo.completed } : todo))
+  }
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+
+    if (todos && todos.length > 0) {
+      setTodos(todos)
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
 
   return (
-    <>
-      <h1 className="text-3xl text-red-600 bg-gray-700">Prince Sunsara</h1>
-    </>
+    <TodoProvider value={{ todos, addTodo, deleteTodo, toggleComplete, updateTodo }}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
   )
 }
 
