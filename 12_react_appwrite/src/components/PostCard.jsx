ff5f19bac444f73c2927d0de727dde3ad6026196
prince-sunsara <princesunasara977@gmail.com>
2024-01-07 22:03:49 +0530

    auth Componenet working

---
+++ 12_react_appwrite/src/components/PostCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import databaseService from '../appwrite/database'
+import { Link } from 'react-router-dom'
+
+
+function PostCard({
+    $id,
+    title,
+    featuredImage
+}) {
+    return (
+        <Link to={`/post/${id}`}>
+            <div className='w-full bg-gray-100 rounded-full p-4'>
+                <div className="w-full justify-center mb-4">
+                    <img
+                        src={databaseService.filePreview(featuredImage)}
+                        alt={title}
+                        className='rounded-xl'
+                    />
+                </div>
+                <h2
+                    className='text-xl font-bold'
+                >
+                    {title}
+                </h2>
+            </div>
+        </Link>
+    )
+}
+
+export default PostCard
--- 12_react_appwrite/src/components/index.js
+++ 12_react_appwrite/src/components/index.js
@@ -1,8 +1,17 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-
+import Container from './container/Container'
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Input from './Input'
+import Button from './Button'
 
 export {
     Footer,
     Header,
+    Container,
+    Logo,
+    LogoutBtn,
+    Input,
+    Button
 }
