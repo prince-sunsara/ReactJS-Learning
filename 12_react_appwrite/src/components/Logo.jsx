cece0e0f4a7bac05e4695429505cc4d77e812354
prince-sunsara <princesunasara977@gmail.com>
2024-01-10 08:36:26 +0530

    changed site name

--- 12_react_appwrite/src/components/Logo.jsx
+++ 12_react_appwrite/src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Logo({ width = '100px' }) {
     return (
-        <div className={`font-bold font-serif text-4xl ${width}`}>Play Circle</div>
+        <div className={`font-bold font-serif text-4xl ${width}`}>Play Blogs</div>
     )
 }
 
